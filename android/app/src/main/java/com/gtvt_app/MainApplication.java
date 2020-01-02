1ed445ea062eac660a30c525302561105b9526cb
tuankhanh45 <tuankhanhtran45@gmail.com>
2020-01-02 16:11:56 +0700

    add test dowload img

--- android/app/src/main/java/com/gtvt_app/MainApplication.java
+++ android/app/src/main/java/com/gtvt_app/MainApplication.java
@@ -4,6 +4,7 @@ import android.app.Application;
 import android.content.Context;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
+import com.RNFetchBlob.RNFetchBlobPackage;
 import com.reactnativecommunity.webview.RNCWebViewPackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 import com.swmansion.reanimated.ReanimatedPackage;
